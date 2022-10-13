248a52e3844180138ab46c01535d3907ca275167
Matheuz-Carvalho <MATHEUS.ACARVALHO@bandtec.com.br>
2022-10-13 20:53:33 -0300

    chat

---
+++ chatte/src/components/logo/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/black.png";
+
+function Logo() {
+    return(
+        <>
+        <img src={logo} alt="LOGO" />
+        </>
+    );
+}
+
+export default Logo;
